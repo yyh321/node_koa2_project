ffe4655021b2cbbef14ab935170dd7aca58b09f1
yyh123 <yunhaoyu@gmail.com>
2018-12-22 20:11:58 +0800

    add views nunjucks2

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const Koa = require('koa')
+const path = require('path')
 const bodyParser = require('koa-bodyparser')
 const router = require('./router')
+const nunjucks = require('koa-nunjucks-2')
 
 const app = new Koa()
 
+app.use(nunjucks({
+    ext: 'html',
+    path: path.join(__dirname, 'views'),
+    nunjucksConfig:{
+        trimBlocks: true
+    }
+}))
+
 app.use(bodyParser())
 router(app)
+
 
 app.listen(3000, ()=>{
     console.log('app is running!')
